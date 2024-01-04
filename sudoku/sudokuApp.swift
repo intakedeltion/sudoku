592bd211817130037b789ef4de1914c950bfc2b9
tijmenhullegie <113439240+tijmenhullegie@users.noreply.github.com>
2024-01-04 16:29:01 +0100

    Initial Commit

---
+++ sudoku/sudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  sudokuApp.swift
+//  sudoku
+//
+//  Created by Tijmen Hullegie on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct sudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
